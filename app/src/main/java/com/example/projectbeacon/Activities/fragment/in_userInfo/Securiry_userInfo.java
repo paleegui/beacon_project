9c19455df6b5d33705a35b4192d661b03f1506b9
paleegui <paleegui@gmail.com>
2019-04-22 15:38:16 +0700

    complete user_Info layout

---
+++ app/src/main/java/com/example/projectbeacon/Activities/fragment/in_userInfo/Securiry_userInfo.java
@@ -0,0 +1,16 @@
+package com.example.projectbeacon.Activities.fragment.in_userInfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projectbeacon.R;
+
+public class Securiry_userInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_securiry_user_info);
+    }
+}
